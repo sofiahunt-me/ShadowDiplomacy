e3e0e6930601e3dfaecf8e0dd8a9a504fc18dca6
sofiahunt-me <LucianiMary341@hotmail.com>
2025-10-17 23:48:47 +0800

    deploy scrypt

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,25 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  if (!process.env.INFURA_API_KEY) {
+    // eslint-disable-next-line no-console
+    console.warn("INFURA_API_KEY is not set. Deployment may fail on public networks.");
+  }
+
+  const deployedContract = await deploy("ShadowDiplomacy", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ShadowDiplomacy contract: `, deployedContract.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_shadow_diplomacy"; // id required to prevent reexecution
+func.tags = ["ShadowDiplomacy"];

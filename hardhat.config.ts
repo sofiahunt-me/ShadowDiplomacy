c30fc5dd6f4fde8f55a4fe94801526ac01768231
sofiahunt-me <LucianiMary341@hotmail.com>
2025-10-18 00:01:34 +0800

    fix deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,16 +6,21 @@ import "@typechain/hardhat";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import type { HardhatUserConfig } from "hardhat/config";
-import { vars } from "hardhat/config";
 import "solidity-coverage";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 import "./tasks/accounts";
 import "./tasks/ShadowDiplomacy";
 
-// Run 'npx hardhat vars setup' to see the list of variables that need to be set
+// Environment variables expected in .env
+// - PRIVATE_KEY: hex string without 0x prefix
+// - INFURA_API_KEY: Infura project id
+// - ETHERSCAN_API_KEY: Etherscan key (optional for verification)
 
-const MNEMONIC: string = vars.get("MNEMONIC", "test test test test test test test test test test test junk");
-const INFURA_API_KEY: string = vars.get("INFURA_API_KEY", "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY ?? "";
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -24,7 +29,7 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      sepolia: vars.get("ETHERSCAN_API_KEY", ""),
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
     },
   },
   gasReporter: {
@@ -34,26 +39,15 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
-      accounts: {
-        mnemonic: MNEMONIC,
-      },
+      // Hardhat local network will use default accounts
       chainId: 31337,
     },
     anvil: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
       chainId: 31337,
       url: "http://localhost:8545",
     },
     sepolia: {
-      accounts: {
-        mnemonic: MNEMONIC,
-        path: "m/44'/60'/0'/0/",
-        count: 10,
-      },
+      accounts: PRIVATE_KEY ? ["0x" + PRIVATE_KEY] : [],
       chainId: 11155111,
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
     },
